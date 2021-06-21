420496a89f2a9bb15fcf2736c00d73a33f73dcd0
andres <a.dev@mailfence.com>
2021-06-21 09:06:19 -0600

    finished ex11.cpp

--- ch3/Exercises/ex11.cpp
+++ ch3/Exercises/ex11.cpp
@@ -2,8 +2,29 @@
 #include <string>
 #include <array>
 // This solution could been easier if std::map were used.
-int main(){
-    constexpr quantity = 5;
-    std::array<std::string, quantity> coinNames { "pennies", "nickels", "dimes", "quarters", "half dollars" };
+
+int main()
+{
+    
+    std::array<std::string, 5> coinNames { "pennie", "nickel", "dime", "quarter", "half dollar" };
+    std::array<int, coinNames.size()> values;
+    
+    for(int i = 0; i < coinNames.size(); i++){
+        std::cout << "How many " << coinNames[i] + "s"<< " do you have?\n";
+        std::cin >> values[i];
+    }
+
+    for (int i = 0; i < coinNames.size(); i++){
+        std::cout << "You have $" << values[i] << ' ';
+
+        if (values[i] == 1)
+        {
+            std::cout << coinNames[i] <<'\n';
+        } 
+        else 
+        {
+            std::cout << coinNames[i] + "s" << '\n';
+        }
+    }
     
 }
