64247744f20e8b7ec05f88b894824d701fb5e857
andres <a.dev@mailfence.com>
2021-06-26 11:35:27 -0600

    Finished ex11

--- ch4/Exercises/ex11.cpp
+++ ch4/Exercises/ex11.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> findPrimes(int max) {
-   std::vector<int> primes;
- 
+std::vector<int> sieveErathostenes(int max) {
+   std::vector<int> primes {2, 3, 5, 7};
+
+   for (int i = 0; i < max; i++) {
+        if (i % 2 && i % 3 && i % 5 && i % 7) { // if is not divisible by any prime
+         primes.push_back(i);
+      }
+   }
    return primes;
 }
 
 int main() {
-   for (const auto& e : findPrimes(100)) {
+   int max; 
+   std::cin >> max;
+   for (const auto& e : sieveErathostenes(max)) {
       std::cout << e << '\n';
    }
    return 0;
