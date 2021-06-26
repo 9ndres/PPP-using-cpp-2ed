e8b829857ba357e562bbce39954eeb311666eee9
andres <a.dev@mailfence.com>
2021-06-26 07:04:34 -0600

    Finished ex2

--- ch4/Exercises/ex2.cpp
+++ ch4/Exercises/ex2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 int main() {
    std::vector<double> temps;
@@ -7,5 +8,18 @@ int main() {
       temps.push_back(temperature);
    }
 
-   
+   std::sort(std::begin(temps), std::end(temps));
+
+   const double center = temps[temps.size() / 2];
+   const double  adjacent = temps[(temps.size() / 2) - 1];
+
+   if (temps.size() % 2) { // If temps is odd the median is in the middle
+      std::cout << "The median equals: " << center << '\n';
+   }
+   else {   // else apply the formula
+      double median = (center + adjacent) / 2;
+      std::cout << "The median equals: " << median << '\n';
+   }
+
+   return 0;
 }
