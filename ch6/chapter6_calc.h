9abe954ca0045d5e5908b340103a7409b6bdd1ab
andres <a.dev@mailfence.com>
2021-07-06 14:46:08 -0600

    add ctor

--- ch6/chapter6_calc.h
+++ ch6/chapter6_calc.h
@@ -16,8 +16,10 @@ class Token {
 public:
 	double value{};
 	char kind;
-	Token(char ch) :kind{ ch } {}
-	Token(char ch, double val) :kind{ ch }, value{ val } {}
+	Token(char ch)
+		:kind{ ch } { }
+	Token(char ch, double val) 
+		:kind{ ch }, value{ val } { }
 };
 
 class Token_stream {
@@ -30,8 +32,40 @@ private:
 	Token buffer;
 };
 
+Token_stream::Token_stream()
+	:full{ false }, buffer{ 0 }
+{
+}
+
 void Token_stream::putback(Token t) {
-	if (full) { BadExpr{ "putback into a full buffer" }; }
-	buffer = t;
+	if (full) { throw BadExpr{ "putback() into a full buffer" }; }
 	full = true;
+	buffer = t;
+}
+
+Token Token_stream::get() {
+	if (full) {
+		full = false;
+		return buffer;
+	}
+
+	char ch;
+	std::cin >> ch;
+
+	switch (ch) {
+	case ';':
+	case 'q':
+	case '(': case ')': case '+': case '-': case '*': case '/':
+		return Token{ ch };
+	case '.':
+	case '0': case '1': case '2': case '3': case '4':
+	case '5': case '6': case '7': case '8': case '9': {
+		std::cin.putback(ch);
+		double val;
+		std::cin >> val;
+		return Token{ '8', val };
+	}
+	default:
+		throw BadExpr{ "Bad token" };
+	}
 }
