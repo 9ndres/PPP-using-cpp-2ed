63f5e60b3a399130fad2a2decc6c2a89f75571dc
andres <a.dev@mailfence.com>
2021-07-14 11:13:50 -0600

    Complete

--- ch6/Exercises/ex10.cpp
+++ ch6/Exercises/ex10.cpp
@@ -3,8 +3,16 @@
 // the permutation or the combination of those two numbers.
 
 #include <iostream>
+#include <string>
 
-class Werr {};
+class Werr {
+public:
+   Werr(std::string w) :w_{w} {}
+   std::string what() { return w_; }
+private:
+   std::string w_ { "Unknown Error" };
+};
+
 int Factorial(int n) {
    while (n > 1) {
       return n * Factorial(n- 1);
@@ -16,17 +24,38 @@ int Permutation(const int N, const int R) {
    return Factorial(N) / Factorial((N - R));
 }
 
+int Combination(const int N, const int R) {
+   return Permutation(N, R) / Factorial(R);
+}
+
 int main() {
    try {
-   int n{}, r{};
-   std::cin >> n >> r;
-   if (r > n) {
-      throw Werr{};
+      int n{}, r{};
+      std::cout << "Enter two integers a and b:\n";
+      std::cin >> n >> r;
+      if (r > n) {
+       throw Werr{ "lhs number greater than rhs number" };
+      }
+      std::cout << "Enter 'p' to calculate the permutation of (a, b)" 
+      R"( or 'c' to get the combination
+)";
+
+      char cho;
+      std::cin >> cho;
+
+      switch(cho) {
+         case 'p':
+         std::cout << Permutation(n, r) << '\n';
+         break;
+         case 'c':
+         std::cout << Combination(n, r) << '\n';
+         break;
+         default:
+         throw Werr{"Not a valid option"};
+      }
    }
-   std::cout << Permutation(n, r) << '\n';
-   }
-   catch(...) {
-      std::cerr << "__ERROR__ lhs number greater than rhs number\n";
+   catch(Werr& e) {
+      std::cerr << "__ERROR__" << e.what() << '\n';
       return -1;
    }
 }
