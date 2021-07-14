eb996089639a4db888f3dc1829f22496b80052f6
andres <a.dev@mailfence.com>
2021-07-14 12:03:21 -0600

    Finalized

--- ch2/Exercises/ex1.cpp
+++ ch2/Exercises/ex1.cpp
@@ -1,6 +1,6 @@
 #include <iostream> // #include directive.
 
-int main() {    // starting function
+int main() {    // starting point 
     std::cout << "Hello programming!\nHere we go!\n"; // output stream
     return 0;
 }
