3bf43415716ae59ba985d33c827e6372177a81d3
andres <a.dev@mailfence.com>
2021-07-15 15:48:35 -0600

    GetValue & SetValue

--- Calculator/Tokenize.h
+++ Calculator/Tokenize.h
@@ -1,6 +1,8 @@
 ﻿#pragma once
 
 #include <iostream>
+#include <string>
+#include <vector>
 
 struct BadExpr {
 	BadExpr(std::string errMessage) :eM{ errMessage } {}
@@ -28,10 +30,34 @@ private:
 	Token buffer;
 };
 
+class Variable {
+public :
+	std::string name;
+	double value{};
+};
 
 inline int factorial(int n) {
 	while (n > 1) {
 		return n * factorial(n - 1);
 	}
 	return 1; // if n == 0
+}
+
+inline double GetValue(const std::string& NAME, const std::vector<Variable>& var_table) {
+	for (const auto& e : var_table) {
+		if (e.name == NAME) {
+			return e.value;
+		}
+		throw BadExpr{ "Undefined variable" };
+	}
+}
+
+inline void SetValue(const std::string& NAME, std::vector<Variable>& var_table, const double& VALUE) {
+	for (auto& e : var_table) {
+		if (e.name == NAME) {
+			e.value = VALUE;
+			return;
+		}
+		throw BadExpr{ "Undefined variable" };
+	}
 }
