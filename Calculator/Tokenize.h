47bda9daaafed346535d3d0f7dcbefa0ac444e00
andres <a.dev@mailfence.com>
2021-07-15 14:43:59 -0600

    Added implementation file

--- Calculator/Tokenize.h
+++ Calculator/Tokenize.h
@@ -1,11 +1,7 @@
-﻿// chapter6_calc.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
+﻿#pragma once
 
 #include <iostream>
 
-// TODO: Reference additional headers your program requires here.
 struct BadExpr {
 	BadExpr(std::string errMessage) :eM{ errMessage } {}
 	std::string what() { return eM; }
@@ -32,40 +28,10 @@ private:
 	Token buffer;
 };
 
-inline Token_stream::Token_stream()
-	:full{ false }, buffer{ 0 }
-{
-}
 
-inline void Token_stream::putback(Token t) {
-	if (full) { throw BadExpr{ "putback() into a full buffer" }; }
-	full = true;
-	buffer = t;
-}
-
-inline Token Token_stream::get() {
-	if (full) {
-		full = false;
-		return buffer;
+inline int factorial(int n) {
+	while (n > 1) {
+		return n * factorial(n - 1);
 	}
-
-	char ch;
-	std::cin >> ch;
-
-	switch (ch) {
-	case ';': case 'q':
-	case '{': case '}':
-	case '(': case ')': case '+': case '-': case '*': case '/': case '!':
-		return Token{ ch };
-	case '.':
-	case '0': case '1': case '2': case '3': case '4':
-	case '5': case '6': case '7': case '8': case '9': {
-		std::cin.putback(ch);
-		double val;
-		std::cin >> val;
-		return Token{ '8', val };
-	}
-	default:
-		throw BadExpr{ "Bad token" };
-	}
+	return 1; // if n == 0
 }
