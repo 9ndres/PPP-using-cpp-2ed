a73c03bf8d3fe13548e6248cf737fdeef044946a
andres <a.dev@mailfence.com>
2021-07-23 10:22:55 -0600

    Commented out swap_cr

--- Chapter08/Drill/Drill02.cpp
+++ Chapter08/Drill/Drill02.cpp
@@ -14,19 +14,20 @@ void swap_r(int& a, int& b) {
    a = b;
    b = temp;
 }
-
+/*
+ // assignment of read only const reference
 void swap_cr(const int& a, const int& b ) {
    int temp;
    temp = a;
    a = b;
    b = temp;
 }
-
+*/
 
 int main() {
-   int x = 7;
-   int y = 9;
- // swap_r(7, 9);
+   int x = 7.7;
+   int y = 9.9;
+   swap_r(x, y);
  // cannot bind non-const lvalue reference of type ‘int&’ 
  // to an    rvalue of type ‘int’
 
