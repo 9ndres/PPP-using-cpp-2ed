64b3c8e6fdbfdf750b03fecf31ec54638f0ca5f7
andres <a.dev@mailfence.com>
2021-07-26 08:58:04 -0600

    Face lift

--- Chapter08/Exercises/Ex02.cpp
+++ Chapter08/Exercises/Ex02.cpp
@@ -2,13 +2,15 @@
 #include <iostream>
 #include <vector>
 
-void print(std::vector<int>& vi, std::string s) {
-   for (const auto& e : vi) {
-      std::cout << s << ' ' << e << '\n';
-   }
+using std::vector;
+
+void print(const vector<int> &vi, std::string s) {
+  for (const auto &e : vi) {
+    std::cout << s << ' ' << e << '\n';
+  }
 }
 
 int main() {
-   std::vector<int> vi { 1, 2, 3, 4, 5, 6, 10000 };
+   vector<int> vi { 1, 2, 3, 4, 5, 6, 10000 };
    print( vi, "vi:");
 }
