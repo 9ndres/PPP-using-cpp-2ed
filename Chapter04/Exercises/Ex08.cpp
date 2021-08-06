77eb37c063c2a66a9631eccba53a55d16e10c9b5
andres <a.dev@mailfence.com>
2021-08-06 16:49:16 -0600

    Exercise needs correction

--- Chapter04/Exercises/Ex08.cpp
+++ Chapter04/Exercises/Ex08.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <cmath>
-
+// correction needed
 int squares(int rice) {
    int i = 0;
    while (pow(2, i) < rice) i++;
    return i;
 }
 int main() {
-   std::cout << squares(100) << '\n'
+   std::cout << squares(3) << '\n'
              << squares(1'000'000) << '\n'
              << squares(100'000'000) << '\n';
    return 0;
