9a2c90ebb884a4af327fc9aa1d49db490f480126
andres <a.dev@mailfence.com>
2021-08-10 16:55:14 -0600

    Drill 03

---
+++ Chapter09/Drill/Drill03.cpp
@@ -0,0 +1,11 @@
+// 08/10/2021
+// Purpose: Same as previous drills
+class Date {
+public:
+    Date(int yy, int mm, int dd) 
+        :y(yy), m(mm), d(dd)
+    {
+    }
+private:
+    int y, m, d; // Keep details private
+};
