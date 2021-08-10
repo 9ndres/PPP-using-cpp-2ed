097ea913c70d03475431d7b87e0e8a662ac7d635
andres <a.dev@mailfence.com>
2021-08-10 14:28:18 -0600

    Completed

--- Chapter09/Drill/Drill01.cpp
+++ Chapter09/Drill/Drill01.cpp
@@ -3,7 +3,21 @@
 
 struct Date {
     // year/month/day
-    int y{};
-    int m{};
-    int d{};
+    int y;
+    int m;
+    int d;
 };
+
+void init_day(Date& d, int yy, int mm, int dd) {
+    // Check if arguments are valid and if so initialize d with them;
+    if (yy > 3000 || yy < 0) { throw; }
+    if (dd > 31 || dd < 1) { throw; }
+    if (mm > 12 || mm < 1) { throw; }
+    d.y{yy};
+    d.m{mm};
+    d.d{dd};
+}
+
+void add_day(Date& d, int n) {
+    d.d += n;
+}
