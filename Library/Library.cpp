f0d8fee2797ea2c1501646320487049a66a45cca
andres <a.dev@mailfence.com>
2021-08-20 18:06:49 -0600

    Implementation

--- Library/Library.cpp
+++ Library/Library.cpp
@@ -1,1 +1,13 @@
 #include "Library.h"
+
+ISBN::ISBN(const long& is) {
+	// Do checking 
+	if (true) {
+		is_valid = true ;
+		barcode = is;
+	}
+}
+Book::Book(const std::string& a, const std::string& t, const ISBN& is)
+	:author(a), title(t), isbn(is)
+{
+}
