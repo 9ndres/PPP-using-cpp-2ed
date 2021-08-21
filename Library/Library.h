3d5fe6866f957388e7ce415b10994d1575ea0ba4
andres <a.dev@mailfence.com>
2021-08-20 18:07:05 -0600

    Header

--- Library/Library.h
+++ Library/Library.h
@@ -1,1 +1,19 @@
 #pragma once
+#include <string>
+
+struct ISBN {
+	ISBN(const long& is);
+private:
+	long barcode{};
+	bool is_valid = false;
+};
+
+struct Book {
+	Book(const std::string& a, const std::string& b, const ISBN& is);
+	std::string get_title() { return title; }
+	std::string get_author() { return author; }
+private:
+	std::string title;
+	std::string author;
+	ISBN isbn;
+};
