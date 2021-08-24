d8f4d90753f28be0d1375901508767feff65a7b0
andres <a.dev@mailfence.com>
2021-08-24 16:16:47 -0600

    For testing purposes

--- Library/main.cpp
+++ Library/main.cpp
@@ -1,6 +1,11 @@
-#include "Library.h"
 #include <string>
+#include "Library.h"
+#include "Date.h"
+#include <iostream>
 
 int main() {
-	Book foo{ "bar", "bas", ISBN{1212121212} };
+	Book num001{ "Shakespeare", "Hamlet", ISBN{ 1212121212 } };
+	Book num002{ "Antoine de Saint-Exupery", "Le Petit Prince", ISBN{ 1212121212 } };
+	auto foo = num001 == num002;
+	std::cout << std::boolalpha << foo << '\n';
 }
